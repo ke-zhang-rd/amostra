b769a4785d32ccaafb6c523684c0328df85435bb
ke-zhang-rd <kezhang@bnl.gov>
2019-08-08 17:24:33 -0400

    MNT: Make test more general

--- amostra/tests/test_examples.py
+++ amostra/tests/test_examples.py
@@ -3,13 +3,12 @@ from hypothesis import given, strategies as st
 from hypothesis.strategies import text
 from hypothesis import settings
 from pymongo import MongoClient
+import random
 
 connection = MongoClient('localhost', 27017)
 db = connection['tests-amostra']
-collection = db['samples']
-collection.drop()
-collection = db['samples_revisions']
-collection.drop()
+db['samples'].drop()
+db['samples_revisions'].drop()
 
 
 client = amostra.mongo_client.Client('mongodb://localhost:27017/tests-amostra')
@@ -19,18 +18,16 @@ def test_one_plus_one_is_two():
     "Check that one and one are indeed two."
     assert 1 + 1 == 2
 
-@given(st.lists(st.characters(whitelist_categories=['Lu']),
-                              min_size=3,
-                              max_size=6,
-                              unique=True))
+alphabet_list = ''
+for i in range(26):
+    alphabet_list = alphabet_list + chr(97 + i)
 
-'''
-Lu referece could be found here
-https://en.wikipedia.org/wiki/Unicode_character_property#General_Category
-'''
-
+@given(st.lists(st.text(alphabet=alphabet_list, min_size=1, max_size=4),
+                min_size=2,
+                max_size=4,
+                unique=True))
 def test_revert(names):
-    print('names', names)
+    print('random names', names)
     n = len(names)
     s = client.samples.new(name = names[0])
     for name in names[1:]:
@@ -39,23 +36,21 @@ def test_revert(names):
 
     assert s.revision == n-1
     assert s.name == names[-1]
-    if n > 1:
-        for i in range(n-1):
-            assert res[i].name == names[n-2-i]
+    for i in range(n-1):
+        assert res[i].name == names[n-2-i]
     '''
     assert res[0].name == 'c'
     assert res[1].name == 'b'
     assert res[2].name == 'a'
     '''
-    num = 1
+    num = random.randint(0, n-2)
     s.revert(num)
     res = list(s.revisions())
 
     assert s.revision == n
     assert s.name == names[n-2-num]
-    if n > 1:
-        for i in range(n):
-            assert res[i].name== names[n-1-i]
+    for i in range(n):
+        assert res[i].name== names[n-1-i]
 
     '''
     assert res[0].name == 'd'
